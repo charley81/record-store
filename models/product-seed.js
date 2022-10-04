91165d9afe83480657dedf19b660cb2dcdd66db7
ChrisKnowsDev <chrisharley81@gmail.com>
2022-10-04 14:20:19 -0400

    added seed data

--- models/product-seed.js
+++ models/product-seed.js
@@ -8,6 +8,96 @@ const productSeedModel = [
       'This beautiful, panoramic view of Ludlow Street in NYC on the album cover of Paul’s Boutique did everything possible to put you right into the Beastie Boys’ world, making it look both funky and inviting. It also made it essential to own the original, fold-out vinyl.',
     image:
       'https://res.cloudinary.com/charley81/image/upload/v1664835724/uptownRecs/fmqzbysw8pheskk5q1q3.jpg'
+  },
+  {
+    artist: 'The Clash',
+    title: 'London Calling',
+    genre: 'Rock',
+    quantity: 0,
+    description:
+      'A rare case where a parody (of the above Elvis cover) becomes a work of art in itself. The effortlessly cool album cover image of bassist Paul Simonon smashing his guitar practically screams rock’n’roll, just like the music inside.',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664841860/uptownRecs/cwdentaicz1ukdgzzmgl.jpg'
+  },
+  {
+    artist: 'Elvis Presley',
+    title: 'Elvis Presley',
+    genre: 'Rock',
+    quantity: 2,
+    description:
+      'RCA wasted no time in cleaning up Elvis, who’d look completely respectable on all future albums. Meanwhile, his debut allowed him to look like the crazed hillbilly everyone’s parents feared he was, captured in mid-song at the Fort Homer Hesterly Armory in Tampa, Florida. Which of course leads us to…',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664841954/uptownRecs/lluota5vyl8zrxwo5a5u.jpg'
+  },
+  {
+    artist: 'The Beatles',
+    title: "St. Pepper's Lonely Hearts Club Band",
+    genre: 'Rock',
+    quantity: 1,
+    description:
+      'Peter Blake’s pop-art assemblage on Sgt. Pepper’s famous album changed record covers forever, and kept many of us occupied for weeks trying to identify everybody at the ceremony.',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664842244/uptownRecs/fexafquel3kdzroprhtp.jpg'
+  },
+  {
+    artist: 'Cheap Thrills',
+    title: 'Big Brother & the Holding Company',
+    genre: 'Rock',
+    quantity: 3,
+    description:
+      'Arguably the coolest 60s album cover of all, the art for Big Brother & the Holding Company’s sophomore record was also most people’s introduction to the style of underground comic art perfected by R. Crumb. This style of art would be associated with psychedelic music from here on out, though Crumb was a bit anti-hippie himself',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664905950/uptownRecs/Big-Brother-And-the-Holding-Company-Cheap-Thrills--1024x1024_ffcbus.jpg'
+  },
+  {
+    artist: 'The Rolling Stones',
+    title: 'Let It Bleed',
+    genre: 'Rock',
+    quantity: 3,
+    description:
+      'This beautiful, panoramic view of Ludlow Street in NYC on the album cover of Paul’s Boutique did everything possible to put you right into the Beastie Boys’ world, making it look both funky and inviting. It also made it essential to own the original, fold-out vinyl.',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664905983/uptownRecs/The-Rolling-Stones-Let-It-Bleed-cover-1024x1024_olojhb.jpg'
+  },
+  {
+    artist: 'Kendrick Lamar',
+    title: 'To Pimp A Butterfly',
+    genre: 'Hip-Hop',
+    quantity: 5,
+    description:
+      'Finding album art that captured the genre-pushing ambition of To Pimp A Butterfly was a tall order, but Kendrick Lamar and TDE were up to the task, as K dot assembled his hometown crew for a victorious party on the White House lawn, stomping on the symbol of a weaponized criminal justice system.',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664906062/uptownRecs/Kendrick-Lamar-To-Pimp-a-Butterfly-1024x1024_pbzq7o.jpg'
+  },
+  {
+    artist: 'Herb Alpert & the Tijuana Brass',
+    title: 'Whipped Cream & Other Delights',
+    genre: 'Rock',
+    quantity: 0,
+    description:
+      'This iconic album cover said it all about coy mid-60s sexuality, bachelor-pad style. Despite its daring appearance, if you looked closely, the whipped-cream clad model was actually wearing a wedding dress.',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664907019/uptownRecs/Herb-Alpert-the-Tijuana-Brass-Whipped-Cream-and-Other-Delights-768x768_gthocz.jpg'
+  },
+  {
+    artist: 'John Coltrane',
+    title: 'Blue Train',
+    genre: 'Blues',
+    quantity: 1,
+    description:
+      'Most of the classic Blue Note covers were full of bright graphics and exuberant photos (and lots of exclamation marks!). Not so with John Coltrane’s Blue Train, whose cool album cover photo and mood lighting marked it as a work to take seriously.',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664907030/uptownRecs/John-Coltrane-Blue-Train-768x768_c9mj1o.webp'
+  },
+  {
+    artist: 'Talking Heads',
+    title: 'Little Creatures',
+    genre: 'Rock',
+    quantity: 5,
+    description:
+      'Howard Finster’s uniquely Southern folk art was a perfect match for Talking Heads’ back-to-roots album (and for R.E.M.’s Reckoning around the same time). While some of Finster’s work had a darker streak, for this album he appropriately chose sunshine and wonderment.',
+    image:
+      'https://res.cloudinary.com/charley81/image/upload/v1664906062/uptownRecs/Talking-Heads-Little-Creatures--768x768_iusrnu.webp'
   }
 ]
 
