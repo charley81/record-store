bb22ab3d0851d0f5eff0a23a724aab61658386ff
ChrisKnowsDev <chrisharley81@gmail.com>
2022-10-03 20:22:24 -0400

    styled products on index

--- views/products/index.js
+++ views/products/index.js
@@ -8,17 +8,28 @@ class Index extends React.Component {
     return (
       <Layout>
         <div className="wrapper">
-          <h1>Create a new product</h1>
-          <section className="product">
+          <h1>All Products</h1>
+          <section className="products">
             {products.map(product => {
-              const { _id, artist, title, image, description, quantity } =
-                product
+              const {
+                _id,
+                artist,
+                title,
+                image,
+                description,
+                quantity,
+                genre
+              } = product
               return (
-                <div key={_id}>
+                <a href={`/products/${_id}`} key={_id} className="item">
                   <img src={image} alt="" />
-                  <h3>{title}</h3>
-                  <h4>{artist}</h4>
-                </div>
+                  <div className="info">
+                    <h3>{artist}</h3>
+                    <p>{title}</p>
+                    <p>Genre: {genre}</p>
+                  </div>
+                  <h3>{quantity} left</h3>
+                </a>
               )
             })}
           </section>
